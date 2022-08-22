c19e85c83a9a623d6de4d6f82201e67bd5b9fbd4
Andrii Stepaniuk <andrewvstepanyuk@gmail.com>
2022-08-22 11:09:33 +0300

    solving 3 and 4 tasks

--- js/task-01.js
+++ js/task-01.js
@@ -12,3 +12,16 @@ arrayCategories.forEach((item) => {
   console.log(`Category: ${item.firstElementChild.textContent}`);
   console.log(`Elements: ${item.lastElementChild.children.length}`);
 });
+
+
+
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
